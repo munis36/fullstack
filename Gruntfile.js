3613a11c6f41823a37288c2448fc51397c1dddc2
mnagarat <munis36@hotmail.com>
2018-02-27 07:41:09 -0500

    grunt - part 1

---
+++ Gruntfile.js
@@ -0,0 +1,46 @@
+/**
+ * Created by mnagarat on 2/27/18.
+ */
+
+'use strict';
+
+module.exports = function(grunt){
+
+    require('time-grunt')(grunt);
+    require('jit-grunt')(grunt);
+
+    grunt.initConfig({
+        sass : {
+            dist : {
+                files : {
+                    'css/syles.css' : 'css/styles.scss'
+                }
+            }
+        },
+        watch: {
+            files: 'css/*.scss',
+            tasks: ['sass']
+        },
+
+        browserSync: {
+            dev: {
+                bsFiles: {
+                    src: [
+                        'css/*.css',
+                        '*.html',
+                        'js/*.js'
+                    ]
+                },
+                options: {
+                    watchTask:true,
+                    server: {
+                        baseDir: './'
+                    }
+                }
+            }
+        }
+    });
+
+    grunt.registerTask('css',['sass']);
+    grunt.registerTask('default', ['browserSync','watch']);
+};
